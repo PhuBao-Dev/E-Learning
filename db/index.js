622fd03e4f95709686294299c8a9086dd5691eeb
tranbaophu <92238818+tranbaophu@users.noreply.github.com>
2022-04-04 18:44:02 +0700

    edit id to connect server

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 async function connect(){
     try {
-        // await mongoose.connect('mongodb://localhost:27017/F8Education');
-        await mongoose.connect('mongodb+srv://admin:123@sneakerjeeps.vk6bp.mongodb.net/ver1');
+        await mongoose.connect('mongodb://localhost:27017/F8Education');
+        // await mongoose.connect('mongodb+srv://admin:123@sneakerjeeps.vk6bp.mongodb.net/ver1');
         console.log("Successfully Connected");
     } catch (error) {
         console.log("Failure Connected!!!!");
@@ -12,7 +12,3 @@ async function connect(){
 }
 
 module.exports = {connect}
-
-
-// mongodb://localhost:27017/F8Education
-// 
